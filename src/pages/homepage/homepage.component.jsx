826eb54cc8d4cc22503095bbd5ac2ea6ab8eef1b
Dang, Mai-Linh <mai-linh.dang@init.de>
2020-11-30 15:56:11 +0100

    Add menu items, directory and homepage source files

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directory from "../../components/directory/directory.component";
+import MenuItem from "../../components/menu-item/menu-item.component";
+
+const HomePage = () => (
+  <div className="Homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
