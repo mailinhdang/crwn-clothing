3ba08c56b3b89ab1701122c4fbd8e314c794f789
Dang, Mai-Linh <mai-linh.dang@init.de>
2020-12-01 16:26:42 +0100

    Update app with shop page with new components

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./homepage.styles.scss";
 import Directory from "../../components/directory/directory.component";
-import MenuItem from "../../components/menu-item/menu-item.component";
 
 const HomePage = () => (
   <div className="Homepage">
